fafe859138a8a88c19f43869086a803279063522
takamasakimura <ryugakimura@icloud.com>
2025-07-30 08:10:34 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import report  # report.py をインポート
+
+report.main()  # これだけで、report.py の画面が表示される
+
+# if not st.session_state.started:
+#     display_base64_gif("gif_assets/start_banner.gif.txt", width=600)
+#     st.markdown("## - tap to start -")
